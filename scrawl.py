5ce76f626649d468907244f63d58f49c17c24e44
Toby Ran <bubblesran@users.noreply.github.com>
2017-06-09 12:06:41 +0800

    initial commit

---
+++ scrawl.py
@@ -0,0 +1,17 @@
+import core
+import model
+
+def get_communitylist():
+	res = []
+	for community in model.Community.select():
+		res.append(community.title)
+	return res
+
+if __name__=="__main__":
+    regionlist = [u'jiangbei',u'yubei',u'yuzhong'] # only pinyin support
+    model.database_init()
+    core.GetCommunityByRegionlist(regionlist) # Init,scrapy celllist and insert database; could run only 1st time
+    communitylist = get_communitylist() # Read celllist from database
+    core.GetHouseByCommunitylist(communitylist)
+    core.GetRentByCommunitylist(communitylist)
+    core.GetSellByCommunitylist(communitylist)
